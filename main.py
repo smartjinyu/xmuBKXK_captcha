6532336b38528e471f78809671ed9485e373e2b3
smartjinyu <smartjinyu@gmail.com>
2017-08-11 22:13:17 +0800

    try to catch exceptions

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         createDir()
 
     count = 0
-    for i in range(1, 101):
+    for i in range(1, 10001):
         if login():
             count = count + 1
         print('{} / {} = {}%'.format(count, i, count / i))
@@ -31,41 +31,47 @@ def login():
     pwd = 'testtest'  # getpass.getpass('Please input the corresponding password:')
     loginUrl = 'http://bkxk.xmu.edu.cn/xsxk/login.html'
     # localInfoUrl = 'http://bkxk.xmu.edu.cn/xsxk/localInfo.html'
-    session = requests.session()
-    captcha_img = session.get('http://bkxk.xmu.edu.cn/xsxk/getCheckCode')
+    try:
+        session = requests.session()
+        captcha_img = session.get('http://bkxk.xmu.edu.cn/xsxk/getCheckCode')
+        rawImage = Image.open(io.BytesIO(captcha_img.content))
+        imgs = processImg.processImg(rawImage)
+        resultCaptcha = []
+        with PyTessBaseAPI() as api:
+            for img in imgs:
+                # img.show()
+                api.SetImage(img)
+                api.SetVariable("tessedit_char_whitelist", "023456789")  # seems that no 1 in captcha
+                api.SetPageSegMode(10)
+                resultCaptcha.append(api.GetUTF8Text())
 
-    rawImage = Image.open(io.BytesIO(captcha_img.content))
-    imgs = processImg.processImg(rawImage)
-    resultCaptcha = []
-    with PyTessBaseAPI() as api:
-        for img in imgs:
-            # img.show()
-            api.SetImage(img)
-            api.SetVariable("tessedit_char_whitelist", "0123456789")
-            api.SetPageSegMode(10)
-            resultCaptcha.append(api.GetUTF8Text())
+        captcha = ''.join(str(x) for x in resultCaptcha).replace('\n', '')
+        print(captcha)
 
-    captcha = ''.join(str(x) for x in resultCaptcha).replace('\n', '')
-    print(captcha)
-
-    loginData = {
-        'username': id,
-        'password': pwd,
-        'checkCode': captcha,
-    }
-    html = session.post(loginUrl, loginData)
-    if u'进入学生选课系统' in html.text:
-        # print('Login successfully!')
-        savePositive(imgs, captcha)
-        return True
-    elif u'用户名或密码错误' in html.text:
-        # print('Wrong id or password!')
-        savePositive(imgs, captcha)
-        return True
-    else:
-        # print('Wrong captcha!')
-        saveNegative(rawImage, captcha)
+        loginData = {
+            'username': id,
+            'password': pwd,
+            'checkCode': captcha,
+        }
+        html = session.post(loginUrl, loginData)
+        if u'进入学生选课系统' in html.text:
+            # print('Login successfully!')
+            savePositive(imgs, captcha)
+            return True
+        elif u'用户名或密码错误' in html.text:
+            # print('Wrong id or password!')
+            savePositive(imgs, captcha)
+            return True
+        else:
+            # print('Wrong captcha!')
+            saveNegative(rawImage, captcha)
+            return False
+    except KeyboardInterrupt:
+        exit(-1)
+    except:
+        print("Something went wrong in this iteration")
         return False
+
 
 
 def createDir():
